987d66b59a2a84bc417d3976ba864da547d86e9c
Regina Yashina <reginayashina@inbox.ru>
2022-05-20 16:46:37 +0300

    feat: add write-file script

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout, exit } = process;
+
+
+
+
+fs.writeFile(
+  path.join(__dirname, 'text.txt'),
+  '',
+
+  (err) => {
+    if (err) throw err;
+    stdout.write('Hey! Write something\n');
+    stdin.on('data', data => {
+      if (data.toString().trim() === 'exit') {
+        exit();
+      }
+      stdout.write(data);
+      fs.appendFile(
+        path.join(__dirname, 'text.txt'),
+        data,
+        err => {
+          if (err) throw err;
+        },
+      );
+    });
+    process.on('exit', () => stdout.write('Thank you! Good luck)'));
+  }
+
+);
