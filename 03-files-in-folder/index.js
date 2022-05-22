a6091a03b8832c32e585be5292cbd71a2d7231d6
Regina Yashina <reginayashina@inbox.ru>
2022-05-22 19:45:20 +0300

    feat: add files-in-folder script

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, 'secret-folder'), (err, files) => {
+  for (let file of files) {
+    fs.stat(path.join(__dirname, 'secret-folder', file), (err, type) => {
+      if (type.isFile()) {
+
+        let name = path.basename(file, path.extname(file));
+        let extname = path.extname(file).slice(1);
+        let size = type.size + 'b';
+        console.log(name + ' - ' + extname + ' - ' + size);
+      }
+    });
+  }
+});
+
